ccfa9aa5b11413823f8aad21983430154d132f9e
sopiandi <ekosopiandi.idris@gmail.com>
2023-06-17 21:33:54 +0800

    pengelolaan data album

---
+++ src/server.js
@@ -0,0 +1,28 @@
+const Hapi = require("@hapi/hapi");
+const albums = require("./api/albums");
+const AlbumsService = require("./services/inMemory/AlbumsService");
+
+const init = async () => {
+  const albumsService = new AlbumsService();
+  const server = Hapi.server({
+    port: 5000,
+    host: process.env.NODE_ENV !== "production" ? "localhost" : "0.0.0.0",
+    routes: {
+      cors: {
+        origin: ["*"],
+      },
+    },
+  });
+
+  await server.register({
+    plugin: albums,
+    options: {
+      service: albumsService,
+    },
+  });
+
+  await server.start();
+  console.log(`Server berjalan pada ${server.info.uri}`);
+};
+
+init();
